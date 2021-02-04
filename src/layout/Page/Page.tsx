1e307e8efba4ad2c8b311dd7d94d233a2ee5d449
Joshua Eture <joshuaeture@gmail.com>
2021-02-04 23:47:44 +0200

    Create Page

---
+++ src/layout/Page/Page.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
+import "./Page.css";
+
+interface PageProps {
+  options: {
+    head: { title: string; description: string };
+    hideConnectButton?: boolean;
+    style?: React.CSSProperties;
+  };
+}
+
+const Page: React.FC<PageProps> = ({ children, options }) => {
+  useEffect(() => {
+    document.title = options.head.title;
+  }, []);
+
+  return (
+    <main id="page" style={options.style}>
+      <Helmet>
+        <title>{options.head.description}</title>
+        <meta name="description" content={options.head.description} />
+      </Helmet>
+      {children}
+    </main>
+  );
+};
+
+export default Page;
