3746497e08517fdfc07021d72f495c586f0d823b
Joshua Eture <joshuaeture@gmail.com>
2021-02-05 02:18:37 +0200

    Complete NotFoundPage * Add Page * Add HeaderTopBar * Add not-found-page-header * Add not-found-page-paragraph

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import Page from "../../layout/Page/Page";
+import HeaderTopBar from "../../navigation/HeaderTopBar/HeaderTopBar";
 import "./NotFoundPage.css";
 
 interface NotFoundPageProps {}
 
 const NotFoundPage: React.FC<NotFoundPageProps> = ({}) => {
-  return <div>NotFoundPage</div>;
+  return (
+    <Page
+      options={{
+        head: {
+          title: "Joshua Eture / Page Not Found",
+          description: "",
+        },
+      }}
+    >
+      <HeaderTopBar options={{ title: "Page Unavailable" }} />
+      <p className="not-found-page-header">Sorry this page isn't available.</p>
+      <p className="not-found-page-paragraph">
+        Return to <a href="/">homepage</a>.
+      </p>
+    </Page>
+  );
 };
 
 export default NotFoundPage;
