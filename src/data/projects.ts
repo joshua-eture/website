ca4055b60935e797e76a2b41512bd38b375ad534
Joshua Eture <joshuaeture@gmail.com>
2021-02-06 01:06:20 +0200

    Add projects data

---
+++ src/data/projects.ts
@@ -0,0 +1,21 @@
+export default [
+  {
+    id: 0,
+    name: "whunt",
+    description: "Google search from terminal with whunt.",
+    link: "https://www.npmjs.com/package/whunt",
+  },
+  {
+    id: 1,
+    name: "numbers-round",
+    description:
+      "Measure your money - keep track of spendings and get insights.",
+    link: "https://github.com/joshua-eture/numbers-round",
+  },
+  {
+    id: 2,
+    name: "website",
+    description: "Source code for my website.",
+    link: "https://github.com/joshua-eture/website",
+  },
+] as const;
