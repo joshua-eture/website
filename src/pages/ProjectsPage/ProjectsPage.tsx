b838d532161005260d385eab4ea8f461bde88f7c
Joshua Eture <joshuaeture@gmail.com>
2021-02-06 01:27:53 +0200

    Complete ProjectsPage * Add HeaderTopBar * Add projects data * Add projects-grid with ProjectItem

--- src/pages/ProjectsPage/ProjectsPage.tsx
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,10 +1,37 @@
 import React from "react";
+import Page from "../../layout/Page/Page";
+import HeaderTopBar from "../../navigation/HeaderTopBar/HeaderTopBar";
+import projects from "../../data/projects";
+import ProjectItem from "../../components/ProjectItem/ProjectItem";
 import "./ProjectsPage.css";
 
 interface ProjectsPageProps {}
 
 const ProjectsPage: React.FC<ProjectsPageProps> = ({}) => {
-  return <div>ProjectsPage</div>;
+  const data = projects;
+
+  return (
+    <Page
+      options={{
+        head: {
+          title: "Joshua Eture / Apps & Projects",
+          description: "Developed by Joshua Eture",
+        },
+      }}
+    >
+      <HeaderTopBar options={{ title: "Apps & Projects" }} />
+      <div id="projects-grid">
+        {data.map((project) => (
+          <ProjectItem
+            key={project.id}
+            name={project.name}
+            description={project.description}
+            link={project.link}
+          />
+        ))}
+      </div>
+    </Page>
+  );
 };
 
 export default ProjectsPage;
