81f0b49286a28364ccae3acb8fed8cab6ea968f1
Joshua Eture <joshuaeture@gmail.com>
2021-02-05 02:00:35 +0200

    Complete HomePage * Add Page component * Add HeaderTopBar * Add homepage-showcase * Add ConnectMenu

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Page from "../../layout/Page/Page";
+import HeaderTopBar from "../../navigation/HeaderTopBar/HeaderTopBar";
+import ConnectMenu from "../../components/ConnectMenu/ConnectMenu";
 import "./HomePage.css";
 
 interface HomePageProps {}
 
 const HomePage: React.FC<HomePageProps> = ({}) => {
-  return <div>Homepage</div>;
+  const MY_AGE = new Date().getFullYear() - 2003;
+  const [connectMenuVisible, setConnectMenuVisible] = useState(false);
+
+  return (
+    <Page
+      options={{
+        head: {
+          title: "Joshua Eture / Website",
+          description: `Computer programmer, ${MY_AGE}`,
+        },
+        style: { overflow: "hidden" },
+      }}
+    >
+      <HeaderTopBar
+        options={{
+          title: "Connect with Me",
+          hideConnectButton: false,
+          whenConnectButtonClicked: () => setConnectMenuVisible(true),
+        }}
+      />
+      <div className="homepage-showcase"></div>
+      <ConnectMenu
+        visible={connectMenuVisible}
+        shouldClose={() => setConnectMenuVisible(false)}
+      />
+    </Page>
+  );
 };
 
 export default HomePage;
