831b247ab26e056eb227a3f8230541d73d97da4c
Joshua Eture <joshuaeture@gmail.com>
2021-02-06 01:39:32 +0200

    Complete PoliciesPage * Add HeaderTopBar * Add policies-text-area * Add no-policy-paragraph

--- src/pages/PoliciesPage/PoliciesPage.tsx
+++ src/pages/PoliciesPage/PoliciesPage.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import Page from "../../layout/Page/Page";
+import HeaderTopBar from "../../navigation/HeaderTopBar/HeaderTopBar";
 import "./PoliciesPage.css";
 
 interface PoliciesPageProps {}
 
 const PoliciesPage: React.FC<PoliciesPageProps> = ({}) => {
-  return <div>PoliciesPage</div>;
+  return (
+    <Page
+      options={{
+        head: {
+          title: "Joshua Eture / Policies",
+          description: "Agreements with Joshua Eture",
+        },
+      }}
+    >
+      <HeaderTopBar options={{ title: "Policies" }} />
+      <div className="policies-text-area">
+        <p id="no-policy-paragraph">
+          <strong>Joshua Eture</strong> doesn't have any policy yet.
+        </p>
+      </div>
+    </Page>
+  );
 };
 
 export default PoliciesPage;
